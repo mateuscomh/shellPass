aaca8aa3526b7cc82d0b192e952088c5278e6575
mateuscomh <3mhenrique@gmail.com>
2020-08-31 20:10:35 -0300

    Case reajustado

--- PassGen.sh
+++ PassGen.sh
@@ -3,54 +3,50 @@
 #-----------HEADER-------------------------------------------------------------|
 # AUTOR             : Matheus Martins 3mhenrique@gmail.com
 # HOMEPAGE          : https://github.com/mateuscomh 
-# DATA CRIAÇÃO      : 29/08/2020 
+# DATA CRIAÃ‡ÃƒO      : 29/08/2020 
 # PROGRAMA          : Shell-Base
-# VERSÃO            : 1.1
-# LICENÇA           : GPL3
-# PEQUENA-DESCRIÇÃO : Programa para criar senhas seguras via terminal
+# VERSÃƒO            : 1.1.3
+# LICENÃ‡A           : GPL3
+# PEQUENA-DESCRIÃ‡ÃƒO : Programa para criar senhas seguras via terminal
 #
 # CHANGELOG :
 #
-# 29/08/2020 18:00 - Adicionada validação de apenas número a ser recebido pelo usuário
-#                  - Adicionado cabeçalho para commit no git
-# 30/08/2020 13:00 - Adicionado status de help e versao
+# 29/08/2020 18:00 - Adicionada validaÃ§Ã£o apenas nÃºmero a ser recebido pelo usuÃ¡rio.
+#                  - Adicionado cabeÃ§alho para commit no git.
+# 30/08/2020 13:00 - Adicionado status de help e versao.
+# 31/08/2020 18:00 - FunÃ§Ã£o case com problemas de repetiÃ§Ã£o e sem fluxo lÃ³gico - corrigido.
 #
 #----------FIM-HEADER---------------------------------------------------------|
 fecha="\033[m"
 verde="\033[32;1m"
 vermelho="\033[31;1m"
 amarelo="\033[01;33m"
-
-case "$1" in
-  -h | --help ) echo "Programa para gerar passwords com complexida alfanumérica e com cacteres especiais rapidamente via terminal"
-               echo "Autor mateuscomh vulgo Django"
-               exit 0 ;;
-  -v | --version ) echo "Versão 1.1"
-                  exit 0 ;;
-  *) echo "$vermelho Opção inválida $fecha"
-      exit 1 ;;
-esac
 #----------FUNCOES------------------------------------------------------------|
 gerarsenha(){
 
-echo "$verde Informe a quantidade de caracteres para a ser gerada $fecha"
+echo "$verde Informe a quantidade de caracteres para a ser gerada ou 0 para sair $fecha"
 read max
 
 case $max in
-    -h | --help) echo "Programa para gerar passwords com complexida alfanumérica e com cacteres especiais rapidamente via terminal"
-               echo "Autor mateuscomh vulgo Django"
-               exit 0 ;;
-  -v | --version) echo "Versão 1.0.2"
-                  exit 0 ;;
-  ''|*[!0-9]*) echo "$vermelho Informe apenas com números o tamanho da senha a ser gerada $fecha" >&2
+  ''|*[!0-9]*) echo "$vermelho Informe apenas com nÃºmeros o tamanho da senha a ser gerada $fecha" >&2
   exit 1;;
+  0) exit 0 ;;
   *)
     echo "$vermelho Senha gerada: $amarelo"
     </dev/urandom tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c $max  ; echo "$fecha" 
     ;;
 esac
 }
-
 #----------FIM-FUNCOES--------------------------------------------------------|
 #Executando...
-gerarsenha
+case "$1" in
+  -h | --help ) echo "Programa para gerar passwords com complexida alfanumï¿½rica e com cacteres especiais rapidamente via terminal"
+               echo "Autor mateuscomh - Django"
+               exit 0 ;;
+  -v | --version ) echo "Versï¿½o 1.1"
+                  exit 0 ;;
+  '') gerarsenha 
+      exit 0;;
+  *) echo "$vermelho Opï¿½ï¿½o invï¿½lida $fecha"
+      exit 1 ;;
+esac
