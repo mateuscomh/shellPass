cb7ff23142637678d43688ce3d2c25d8119701f4
Matheus Martins <matheuscumth@gmail.com>
2022-06-01 12:16:37 -0300

    Update: suport added to MACos on copyclippboard

--- passgen.sh
+++ passgen.sh
@@ -2,90 +2,95 @@
 
 #-----------HEADER-------------------------------------------------------------|
 # AUTOR             : Matheus Martins <3mhenrique@gmail.com>
-# HOMEPAGE          : https://github.com/mateuscomh/Pass_Gen_Shell
-# DATA              : 29/08/2020 
-# VERSÃO            : 1.7.2
+# HOMEPAGE          : https://github.com/mateuscomh
+# DATA/VER.         : 29/08/2020 1.8
 # LICENÇA           : GPL3
-# PEQUENA-DESCRICÃO : Programa para criar senhas via terminal shell
+# PEQUENA-DESCRICÃO : Script em shell para criação de senhas
+# REQUISITOS        : xclip
 
-#----------FIM-HEADER---------------------------------------------------------|
-fecha='\033[m'
-verde='\033[32;1m'
-vermelho='\033[31;1m'
-amarelo='\033[01;33m'
-max=$1
-
-#----------FUNCOES------------------------------------------------------------|
+#----------FIM-HEADER----------------------------------------------------------|
+FECHA='\033[m'
+VERDE='\033[32;1m'
+VERMELHO='\033[31;1m'
+AMARELO='\033[01;33m'
+MAX=$1
+export LANG=C
+#export LC_CTYPE=C
+#----------FUNCOES-------------------------------------------------------------|
 _gerarsenha(){
-if [ -z "$max" ] || [ "$max" -eq 0 ]; then
-  echo -e "${verde} Informe a QUANTIDADE de caracteres para senha: ${fecha}"
-  read -r max
+if [ -z "$MAX" ] || [ "$MAX" -eq 0 ]; then
+  echo -e "${VERDE} Informe a QUANTIDADE de caracteres para senha: ${FECHA}"
+  read -r MAX
 fi
 
-case $max in
-  ''|*[!0-9]*) 
-    echo -e "${vermelho} Insira apenas números referente ao TAMANHO da senha ${fecha}" 
-    return 1 
+case $MAX in
+  ''|*[!0-9]*)
+    echo -e "${VERMELHO} Insira apenas números referente ao TAMANHO da senha ${FECHA}"
+    return 1
     ;;
-  [0-9]*) 
-    echo -e "${verde} Informe o TIPO da complexidade de senha que deseja: ${fecha}
-    ${amarelo} 1 - Senha apenas números ${fecha}
-    ${amarelo} 2 - Senha com LeTrAs e números ${fecha}
-    ${amarelo} 3 - Senha com LeTrAs, números e caracteres especiais ${fecha}"
-    read -r tipo
-    
-  case "$tipo" in
-    ''|*[!0-9]*) 
-      echo -e "${vermelho} Insira apenas números referente ao TIPO da senha ${fecha}" 
+  [0-9]*)
+    echo -e "${VERDE} Informe o TIPO da complexidade de senha que deseja: ${FECHA}
+    ${AMARELO} 1 - Senha apenas números ${FECHA}
+    ${AMARELO} 2 - Senha com LeTrAs e números ${FECHA}
+    ${AMARELO} 3 - Senha com LeTrAs, números e caracteres especiais ${FECHA}"
+    read -r TIPO
+
+  case "$TIPO" in
+    ''|*[!0-9]*)
+      echo -e "${VERMELHO} Insira apenas números referente ao TIPO da senha ${FECHA}"
       return 2
       ;;
     1)
-      echo -e "${vermelho} Senha gerada: ${amarelo}"
-      </dev/urandom LC_ALL=C tr -dc '0-9' | head -c "$max"  ; echo -e "${fecha}"
+      PASS=$(cat /dev/urandom LC_ALL=C | tr -dc '0-9' | head -c "$MAX")
+      command -v xclip && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy
+      echo -e "${VERDE}$PASS${FECHA}"
       ;;
     2)
-      echo -e "${vermelho} Senha gerada: ${amarelo}"
-      </dev/urandom LC_ALL=C tr -dc 'A-Za-z0-9' | head -c "$max"  ; echo -e "${fecha}"
+      PASS=$(cat /dev/urandom LC_ALL=C | tr -dc 'A-Za-z0-9' | head -c "$MAX")
+      command -v xclip && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy
+      #echo -n "$PASS" | xclip -sel copy
+      echo -e "${VERDE}$PASS${FECHA}"
       ;;
     3)
-      echo -e "${vermelho} Senha gerada: ${amarelo}"
-      </dev/urandom LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$max"
-      echo -e "${fecha}" 
+      PASS=$(cat /dev/urandom LC_ALL=C |
+        tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$MAX")
+      command -v xclip && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy
+      echo -e "${VERDE}$PASS${FECHA}"
       ;;
     *)
-      echo -e "${vermelho} Utilizar as opcoes [4,2,3] ${fecha}"
-      return 1 
+      echo -e "${VERMELHO} Utilizar as opcoes [4,2,3] ${FECHA}"
+      return 1
       ;;
   esac
 esac
 }
 
-#----------FIM-FUNCOES--------------------------------------------------------|
-case "$max" in
-  -h | --help ) 
-    echo -e "${verde} Programa para gerar passwords com complexidade
-    alfanumérica e com caracteres especiais rapidamente via terminal ${fecha}"
-    echo -e "${verde} Autor mateuscomh vulgo Django ${fecha}"
-    exit 0 
+#----------FIM-FUNCOES---------------------------------------------------------|
+case "$MAX" in
+  -h | --help )
+    echo -e "${VERDE} Programa para gerar passwords com complexidade
+    alfanumérica e com caracteres especiais rapidamente via terminal ${FECHA}"
+    echo -e "${VERDE} Autor mateuscomh vulgo Django ${FECHA}"
+    exit 0
     ;;
-  -v | --version ) 
-    echo -e "${verde} Versão 1.7 ${fecha}"
-    exit 0 
+  -v | --version )
+    echo -e "${VERDE} Versão 1.7 ${FECHA}"
+    exit 0
     ;;
   '')
     OP=s
     while true; do
       if [[ "$OP" = [sS] ]]; then
-        max=0
+        MAX=0
       _gerarsenha
         read -n 1 -p "Deseja gerar nova senha? [s/n]" OP; echo
       elif [[ "$OP" = [nN] ]]; then
         break
-      else 
-        echo -e "${vermelho} Opção inválida ${fecha}"
+      else
+        echo -e "${VERMELHO} Opção inválida ${FECHA}"
         read -n 1 -p "Deseja gerar nova senha? [s/n]" OP; echo
       fi
-    done 
+    done
     ;;
   [0-9]*)
     _gerarsenha
