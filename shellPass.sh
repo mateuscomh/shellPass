af7f88b3339387e248ecb074d5bfd29ac2b80dce
Matheus Martins <matheuscumth@gmail.com>
2022-09-21 13:33:36 -0300

    Update header, print functions and rename

--- shellPass.sh
+++ shellPass.sh
@@ -2,20 +2,20 @@
 
 #------------------------------------------------------------------------------|
 # AUTOR             : Matheus Martins <3mhenrique@gmail.com>
-# HOMEPAGE          : https://github.com/mateuscomh
-# DATA/VER.         : 29/08/2020 2.4
+# HOMEPAGE          : https://github.com/mateuscomh/shellPass
+# DATE/VER.         : 29/08/2020 2.4
 # LICENCE           : GPL3
 # SHORT DESC        : Shell Script to generate fast passwords on terminal
-# REQ               : xclip on GNU/Linux / pbcopy on MacOS
+# DEPS              : xclip on GNU/Linux / pbcopy on MacOS
 
 #------------------------------------------------------------------------------|
 export LANG=C
-VERSION='2.4'
-USAGE=$(echo "Program to generate on shell passwords whith alpanum on terminal
+VERSION='2.4 by Matheus Martins'
+USAGE="Program to generate on shell passwords whith alpanum on terminal
 
 ░▒█▀▀▀█░█░░░░█▀▀░█░░█░░▄▀▀▄░█▀▀▄░█▀▀░█▀▀
 ░░▀▀▀▄▄░█▀▀█░█▀▀░█░░█░░█▄▄█░█▄▄█░▀▀▄░▀▀▄
-░▒█▄▄▄█░▀░░▀░▀▀▀░▀▀░▀▀░█░░░░▀░░▀░▀▀▀░▀▀▀ ")
+░▒█▄▄▄█░▀░░▀░▀▀▀░▀▀░▀▀░█░░░░▀░░▀░▀▀▀░▀▀▀ "
 FECHA='\033[m'
 VERDE='\033[32;1m'
 VERMELHO='\033[31;1m'
@@ -32,6 +32,10 @@ if [ -z "$MAX" ] || [ "$MAX" -eq 0 ]; then
 fi
 
 case $MAX in
+  q|Q)
+    echo -e "Quiting..."
+    exit 0
+    ;;
   ''|*[!0-9]*)
     echo -e "${VERMELHO} Enter only numbers referring to the SIZE of the password ${FECHA}"
     return 1
@@ -43,32 +47,32 @@ case $MAX in
     ${AMARELO} 3 - Password with LeTtErS, numb3rs and Speci@l Ch@r@ct&rs ${FECHA}"
     read -r TIPO
 
-  case "$TIPO" in
-    ''|*[!0-9]*)
-      echo -e "${VERMELHO} Enter only numbers referring to the TYPE of the password ${FECHA}"
-      return 2
-      ;;
-    1)
-      PASS=$(cat /dev/urandom LC_ALL=C | tr -dc '0-9' | head -c "$MAX")
-      command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-      echo -e "${VERDE}$PASS${FECHA}"
-      ;;
-    2)
+    case "$TIPO" in
+      ''|*[!0-9]*)
+        echo -e "${VERMELHO} Enter only numbers referring to the TYPE of the password ${FECHA}"
+        return 2
+        ;;
+      1)
+        PASS=$(cat /dev/urandom LC_ALL=C | tr -dc '0-9' | head -c "$MAX")
+        command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
+        echo -e "${VERDE}$PASS${FECHA}"
+        ;;
+      2)
       PASS=$(cat /dev/urandom LC_ALL=C | tr -dc 'A-Za-z0-9' | head -c "$MAX")
       command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
       echo -e "${VERDE}$PASS${FECHA}"
       ;;
-    3)
-      PASS=$(cat /dev/urandom LC_ALL=C |
-        tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$MAX")
-      command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-      echo -e "${VERDE}$PASS${FECHA}"
-      ;;
-    *)
-      echo -e "${VERMELHO} Use only the options [1,2,3] ${FECHA}"
-      return 1
-      ;;
-  esac
+      3)
+        PASS=$(cat /dev/urandom LC_ALL=C |
+          tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$MAX")
+        command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
+        echo -e "${VERDE}$PASS${FECHA}"
+        ;;
+      *)
+        echo -e "${VERMELHO} Use only the options [1,2,3] ${FECHA}"
+        return 1
+        ;;
+    esac
 esac
 # write in file
 echo "$(date '+%d/%m/%y %H:%M:%S') - $PASS" >> $(pwd)/history.log
@@ -77,11 +81,11 @@ echo "$(date '+%d/%m/%y %H:%M:%S') - $PASS" >> $(pwd)/history.log
 #---------MAIN-------------------------------------------------------------------|
 
 case "$MAX" in
-  -h | --help )
-    echo -e "${VERDE} $USAGE ${FECHA}"
+  h | -h | --help )
+    echo -e "$USAGE"
     exit 0
     ;;
-  -v | --version )
+  v | -v | --version )
     echo -e "${VERDE} $VERSION ${FECHA}"
     exit 0
     ;;
