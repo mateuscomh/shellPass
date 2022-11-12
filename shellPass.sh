f225a2ab91f70b7f9e83a1e7bccaa031a2ecd487
Django <matheuscumth@gmail.com>
2022-11-12 20:19:21 -0300

    refactoring

--- shellPass.sh
+++ shellPass.sh
@@ -16,59 +16,57 @@ USAGE="Program to generate on shell passwords whith alpanum on terminal
 ░░▀▀▀▄▄░█▀▀█░█▀▀░█░░█░░█▄▄█░█▄▄█░▀▀▄░▀▀▄
 ░▒█▄▄▄█░▀░░▀░▀▀▀░▀▀░▀▀░█░░░░▀░░▀░▀▀▀░▀▀▀ "
 FECHA='\033[m'
-VERDE='\033[32;1m'
-VERMELHO='\033[31;1m'
-AMARELO='\033[01;33m'
+BOLD=$(tput bold)
+BLINK=$(tput blink)
+ITALIC=$(tput dim)
 MAX=$1
 #----------FUNC-------------------------------------------------------------|
-
-_gerarsenha(){
-if [ -z "$MAX" ] || [ "$MAX" -eq 0 ]; then
-  clear
+clear
+_gettamanho(){
   echo -e "$USAGE \n"
-  echo -e "${VERDE} Enter the QUANTITY of characters for the password or [Q] to quit: ${FECHA}"
+  echo -e "${BOLD} Enter the QUANTITY of characters for the password or [Q] to quit: ${FECHA}"
   read -r MAX
-fi
-
+}
+_gerarsenha(){
 case $MAX in
   q|Q)
     echo -e "Quiting..."
     exit 0
     ;;
   ''|*[!0-9]*)
-    echo -e "${VERMELHO} Enter only numbers referring to the SIZE of the password ${FECHA}"
-    return 1
+    echo -e "${BLINK} Invalid option. ${FECHA}"
+    _gettamanho
     ;;
   [0-9]*)
-    echo -e "${VERDE} Enter the TYPE of password complexity you want: ${FECHA}
-    ${AMARELO} 1 - Password only numbers ${FECHA}
-    ${AMARELO} 2 - Password with LeTtErS and numb3rs ${FECHA}
-    ${AMARELO} 3 - Password with LeTtErS, numb3rs and Speci@l Ch@r@ct&rs ${FECHA}";
-    read -sn 1 TIPO;
+    echo -e "${BOLD} Enter the TYPE of password complexity you want: ${FECHA}
+    ${ITALIC} 1 - Password only numbers ${FECHA}
+    ${ITALIC} 2 - Password with LeTtErS and numb3rs ${FECHA}
+    ${ITALIC} 3 - Password with LeTtErS, numb3rs and Speci@l Ch@r@ct&rs ${FECHA}";
+    read -rsn 1 TIPO;
 
     case "$TIPO" in
       ''|*[!0-9]*)
-        echo -e "${VERMELHO} Enter only numbers referring to the TYPE of the password ${FECHA}"
+        echo -e "${BLINK} Enter only numbers referring to the TYPE of the password ${FECHA}"
         return 2
         ;;
       1)
         PASS=$(cat /dev/urandom LC_ALL=C | tr -dc '0-9' | head -c "$MAX")
         command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-        echo -e "${VERDE}$PASS${FECHA}"
+        echo -e "${BOLD}$PASS${FECHA}"
         ;;
       2)
       PASS=$(cat /dev/urandom LC_ALL=C | tr -dc 'A-Za-z0-9' | head -c "$MAX")
       command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-      echo -e "${VERDE}$PASS${FECHA}"
+      echo -e "${BOLD}$PASS${FECHA}"
       ;;
       3)
         PASS=$(cat /dev/urandom LC_ALL=C |
           tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$MAX")
         command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-        echo -e "${VERDE}$PASS${FECHA}"
+        echo -e "${BOLD}$PASS${FECHA}"
         ;;
       *)
-        echo -e "${VERMELHO} Use only the options [1,2,3] ${FECHA}"
+        echo -e "${BLINK} Use only the options [1,2,3] ${FECHA}"
         return 1
         ;;
     esac
@@ -88,7 +86,7 @@ case "$MAX" in
     exit 0
     ;;
   v | -v | --version )
-    echo -e "${VERDE} $VERSION ${FECHA}"
+    echo -e "${BOLD} $VERSION ${FECHA}"
     exit 0
     ;;
   '')
@@ -96,15 +94,17 @@ case "$MAX" in
     while true; do
       if [[ "$OP" = [yYsS] ]]; then
         MAX=0
+      _gettamanho
       _gerarsenha
       _askprint
       elif [[ "$OP" = [rR] ]]; then
+        _gettamanho
         _gerarsenha
         _askprint
       elif [[ "$OP" = [nNqQ] ]]; then
         break
       else
-        echo -e "${VERMELHO} Invalid option ${FECHA}"
+        echo -e "${BLINK} Invalid option ${FECHA}"
         _askprint
       fi
     done
